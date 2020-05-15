5029ce27bfba463772565dd42b4993c207176d5c
opdshe <50160282+opdshe@users.noreply.github.com>
2020-05-15 23:30:25 +0900

    feat : Add Sample Controller, DTO, test code

---
+++ src/test/java/com/dongheon/springboot/web/HelloControllerTest.java
@@ -0,0 +1,43 @@
+package com.dongheon.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void 헬로_메서드_확인() throws Exception {
+        String hello = "hello";
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void 헬로DTO_메서드_확인() throws Exception {
+        //when
+        String name = "dongheon";
+        int amount = 1000;
+
+        //then
+        mockMvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
+
+    }
+}
---
+++ src/test/java/com/dongheon/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.dongheon.springboot.web.dto;
+
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name  = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto helloResponseDto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(helloResponseDto.getName()).isEqualTo(name);
+        assertThat(helloResponseDto.getAmount()).isEqualTo(amount);
+    }
+
+
+}
