c3e8b240b8173e276366197014d87a6af830c767
opdshe <50160282+opdshe@users.noreply.github.com>
2020-05-21 10:54:04 +0900

    test : JPA Auditing 기능 테스트 코드 작성

--- src/test/java/com/dongheon/springboot/domain/posts/PostRepositoryTest.java
+++ src/test/java/com/dongheon/springboot/domain/posts/PostRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,4 +43,26 @@ public class PostRepositoryTest {
         assertThat(post.getTitle()).isEqualTo(title);
         assertThat(post.getContent()).isEqualTo(content);
     }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of (2020,5,21,10,10,10);
+        postRepository.save(Post.builder()
+        .title("title")
+        .content("content")
+        .author("author")
+        .build());
+
+        // when
+        List<Post> postList = postRepository.findAll();
+
+        // then
+        Post post = postList.get(0);
+        System.out.println(">>>>>>>>>>createDate = " + post.getCreateDate() +
+                ", modifiedDate = " + post.getModifiedDate());
+        assertThat(post.getCreateDate()).isAfter(now);
+        assertThat(post.getModifiedDate()).isAfter(now);
+    }
+
 }
